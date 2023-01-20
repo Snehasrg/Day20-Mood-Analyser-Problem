54bf13bd4bb46a58408ba4c8761418e64612dc99
Snehasrg <sghongade642@gmail.com>
2023-01-20 18:08:46 +0530

    using refactor take message in constructor

--- MoodAnalyser.cs
+++ MoodAnalyser.cs
@@ -1,45 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MoodAnalyserProject
+﻿namespace MoodAnalyserProject
 {
     public class MoodAnalyser
     {
         string message;
-
-        public MoodAnalyser()
+        public MoodAnalyser(string Message)
         {
-            Console.WriteLine("Default constructor");
-        }
-
-        public MoodAnalyser(string message)
-        {
-            this.message = message;
+            this.message = Message;
         }
         public string AnalyzeMood()
         {
-            try
+            if (message.ToLower().Contains("happy"))
             {
-                if (message.Equals(string.Empty))
-                {
-                    throw new CustomException(CustomException.ExceptionType.EMPTY_EXCEPTION, "Mood should not be empty");
-                }
-                else if (message.ToLower().Contains("happy"))
-                {
-                    return "happy";
-                }
-                else
-                {
-                    return "sad";
-                }
+                return "happy";
             }
-            catch (NullReferenceException)
+            else
             {
-                throw new CustomException(CustomException.ExceptionType.NULL_EXCEPTION, "Mood should not be NULL");
-
+                return "sad";
             }
         }
 
